02468eb8f73c6164c5884107bd8743ca029cc106
sashee <tamas.sallai@advancedweb.hu>
2025-08-22 17:52:16 +0200

    dataviz

--- data-viz/data-viz.ts
+++ data-viz/data-viz.ts
@@ -1,19 +1,57 @@
 import {DatabaseSync} from "node:sqlite";
 import path from "node:path";
 import express from "express";
+
 const app = express();
 const port = 8080;
 
-const database = new DatabaseSync(path.join(process.env.HOME, "monitoring-data", "data.db"));
+const dbPath = "data.db";
+//const dbPath = path.join(process.env.HOME, "monitoring-data", "data.db");
 
-const readAll = database.prepare("SELECT json_extract(value, '$.inverter.qpigs.ac_output_voltage') FROM data");
+const database = new DatabaseSync(dbPath, {readOnly: true});
 
-app.get('/', (req, res) => {
-	console.log(readAll.all());
-  res.send('Hello World!')
+const statements = {
+	simple: database.prepare("SELECT timestamp, json_extract(value, :value) as data FROM data WHERE data is not null AND timestamp >= :from AND timestamp <= :to;")
+};
+
+const latestValue = JSON.parse(database.prepare("SELECT value FROM data ORDER BY timestamp desc LIMIT 1;").get().value);
+//console.log(latestValue);
+
+Object.entries(latestValue).flatMap(([tl, tv]) => {
+	return Object.entries(tv).flatMap(([ll, lv]) => {
+		return Object.entries(lv).flatMap(([kl, kv]) => {
+		if (["number", "boolean"].includes(typeof kv)) {
+			//console.log(`<div class="chart"\n\tdata-series-1='{"type": "simple", "title": "${tl}/${ll}/${kl}", "params": {"value": "$.${tl}.${ll}.${kl}"}}'\n\tdata-title="${tl}/${ll}/${kl}"\n></div>`)
+		}else {
+			if (typeof kv === "object" && Object.values(kv).every((v) => typeof v === "boolean")) {
+				Object.keys(kv).map((v) => {
+					//console.log(`<div class="chart"\n\tdata-series-1='{"type": "simple", "title": "${tl}/${ll}/${kl}/${v}", "params": {"value": "$.${tl}.${ll}.${kl}[${v}]"}}'\n\tdata-title="${tl}/${ll}/${kl}/${v}"\n></div>`)
+				})
+			}else {
+			console.log(tl, ll, kl, typeof kv, kv, Array.isArray(kv))
+
+			}
+		}
+		})
+	})
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use(express.static('static'))
+
+app.get('/sql', (req, res) => {
+	console.log(req.query.sql)
+	console.log(req.query.parameters)
+  res.send(statements[req.query.sql].all(JSON.parse(req.query.parameters)))
+});
+
+await new Promise((resolve, reject) => {
+	app.listen(port, (e) => {
+		if (e) {
+			reject(e);
+		}
+		console.log(`Example app listening on port ${port}`)
+	});
+	app.on('close', () => resolve(undefined));
+});
+
 
---
+++ data-viz/static/script.js
@@ -0,0 +1,84 @@
+const echarts = await import("https://esm.sh/echarts@6.0.0");
+
+await Promise.all([...document.querySelectorAll(".chart")].map(async (e) => {
+	const observer = new IntersectionObserver((entries, observer) => {
+		entries.filter((entry) => entry.isIntersecting).forEach(async () => {
+			observer.unobserve(e);
+			const sql = e.dataset.sql;
+			console.log(sql)
+			console.log((e.dataset))
+			const timePeriod = {
+				from: new Date(new Date().getTime() - 1000*60*60*24*7).getTime(),
+				to: new Date().getTime(),
+			};
+			const datas = await Promise.all(Object.entries(e.dataset).filter(([n]) => n.startsWith("series-")).map(async ([, series]) => {
+				const {type, title, params} = JSON.parse(series);
+				const res = await fetch(`/sql?sql=${encodeURIComponent(type)}&parameters=${encodeURIComponent(JSON.stringify({...params, ...timePeriod}))}`);
+				if (!res.ok) {
+					console.error(res);
+					throw new Error("fetch failed");
+				}
+				const data = await res.json();
+				return {data, title};
+			}));
+			const uniqueTimestamps = datas.flatMap(({data}) => data.flatMap(({timestamp}) => timestamp)).filter((e, i, l) => l.indexOf(e) ===i).toSorted();
+			const mergedData = uniqueTimestamps.map((timestamp) => {
+				return [
+					new Date(timestamp).toISOString(),
+					...datas.map(({data}) => data.find((d) => d.timestamp === timestamp)?.data ?? null),
+				];
+			});
+
+			var myChart = echarts.init(e);
+myChart.setOption({
+	dataset: {
+		source: mergedData,
+		dimensions: ['timestamp', ...datas.map((_d, i) => `data_${i}`)],
+	},
+	animation: false,
+	grid: {
+    left: 0,
+    right: 0,
+    top: 50,
+    bottom: 0,
+  },
+	title: { text: e.dataset.title},
+	tooltip: {
+		trigger: 'axis',
+		axisPointer: {
+			type: 'line',
+			lineStyle: {
+				color: '#aaa',
+				width: 1
+			}
+		}
+	},
+  xAxis: {
+    type: 'time',
+		min: new Date(timePeriod.from),
+		max: new Date(timePeriod.to),
+  },
+  yAxis: {
+    type: 'value'
+  },
+	series: datas.map(({title}, i) => {
+		return {
+			name: title,
+			type: 'line',
+			encode: {
+				x: "timestamp",
+				y: `data_${i}`
+			},
+			symbol: "none",
+		}
+	}),
+});
+					console.log("RENDERED")
+		});
+	}, {
+		rootMargin: "0px",
+		scrollMargin: "0px",
+		threshold: 0,
+	});
+	observer.observe(e);
+}));
