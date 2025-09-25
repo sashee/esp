ca4c047e79dcc079fccd2e4007e93c4b5cb1bb22
sashee <tamas.sallai@advancedweb.hu>
2025-09-25 18:16:48 +0200

    power charts

--- data-viz/queries.ts
+++ data-viz/queries.ts
@@ -65,16 +65,32 @@ const extractTwoFromData = (namedParameter1: string, namedParameter2: string) =>
 SELECT timestamp, json_extract(value, :${namedParameter1}) as data1, json_extract(value, :${namedParameter2}) as data2 FROM data
 `;
 
-const sumTwoData = (query: string) => `
-SELECT timestamp, ifnull(data1, 0) + ifnull(data2, 0) as data FROM (${increaseIndent(query)})
+const extractFiveFromData = (resultColum1: string = "data1", resultColum2: string = "data2", resultColum3: string = "data3", resultColum4: string = "data4", resultColum5: string = "data5") => (namedParameter1: string, namedParameter2: string, namedParameter3: string, namedParameter4: string, namedParameter5: string) => () => `
+SELECT timestamp, json_extract(value, :${namedParameter1}) as ${resultColum1}, json_extract(value, :${namedParameter2}) as ${resultColum2}, json_extract(value, :${namedParameter3}) as ${resultColum3}, json_extract(value, :${namedParameter4}) as ${resultColum4}, json_extract(value, :${namedParameter5}) as ${resultColum5} FROM data
+`;
+
+const multiply = (extraColumns: string[] = [], columnName: string = "data") => (amount: number) => (query: string) => `
+SELECT ${["timestamp", `${columnName} * ${amount} as ${columnName}`, ...extraColumns].join(", ")} fROM (${increaseIndent(query)})
+`;
+
+const sumTwoData = (extraColumns: string[] = [], resultColumnName: string = "data") => (column1Name: string, column2Name: string) => (query: string) => `
+SELECT ${["timestamp", `ifnull(${column1Name}, 0) + ifnull(${column2Name}, 0) as ${resultColumnName}`, ...extraColumns].join(", ")} FROM (${increaseIndent(query)})
+`;
+
+const sumThreeData = (extraColumns: string[] = [], resultColumnName: string = "data") => (column1Name: string, column2Name: string, column3Name: string) => (query: string) => `
+SELECT ${["timestamp", `ifnull(${column1Name}, 0) + ifnull(${column2Name}, 0) + ifnull(${column3Name}, 0) as ${resultColumnName}`, ...extraColumns].join(", ")} FROM (${increaseIndent(query)})
 `;
 
 const threestate = (zeroNamedParameter: string, oneNamedParameter: string) => (query: string) => `
 SELECT timestamp, CASE data WHEN :${zeroNamedParameter} then 0 WHEN :${oneNamedParameter} THEN 1 ELSE 2 END as data FROM (${increaseIndent(query)})
 `;
 
-const multipleWithSign = (dataName: string, signName: string) => (query: string) => `
-SELECT timestamp, (${dataName} * ifnull(sign(${signName}), 0)) as data FROM (${increaseIndent(query)})
+const multipleWithSign = (extraColumns: string[] = [], resultColumnName: string = "data") => (dataName: string, signName: string) => (query: string) => `
+SELECT ${["timestamp", `(${dataName} * ifnull(sign(${signName}), 0)) as ${resultColumnName}`, ...extraColumns].join(", ")} FROM (${increaseIndent(query)})
+`;
+
+const round = (extraColumns: string[] = [], resultColumnName: string = "data") => (columnName: string = "data", decimals: number = 0) => (query: string) => `
+SELECT ${["timestamp", `round(${columnName}, ${decimals}) as ${resultColumnName}`, ...extraColumns].join(", ")} FROM (${increaseIndent(query)})
 `;
 
 const derivation = (query: string) => `
@@ -103,15 +119,34 @@ FROM (
 )
 `;
 
+const teljesitmenyek = {
+	battery_watt: [extractTwoFromData("value", "sign"), dropNullData("data1"), multipleWithSign()("data1", "data2"), multiply()(-1)],
+	pv: [extractFromData("value"), dropNullData()],
+	pv_sum: [extractTwoFromData("value1", "value2"), sumTwoData()("data1", "data2")],
+	active_power: [extractFromData("value"), dropNullData(), multiply()(-1)],
+	sum: [extractFiveFromData("pv1", "pv2", "battery_watt", "battery_sign", "ac_output")("pv1", "pv2", "battery_watt", "battery_sign", "ac_output"), sumTwoData(["battery_watt", "battery_sign", "ac_output"], "pv_sum")("pv1", "pv2"), multipleWithSign(["pv_sum", "ac_output"], "battery_watt_signed")("battery_watt", "battery_sign"), multiply(["pv_sum", "ac_output"], "battery_watt_signed")(-1), multiply(["pv_sum", "battery_watt_signed"], "ac_output")(-1), sumThreeData([], "data")("pv_sum", "battery_watt_signed", "ac_output")],
+
+};
+
 const queries = {
 	simple: flow(extractFromData("value"), dropNullData(), timerange),
 	three_state_value: flow(extractFromData("value"), dropNullData(), threestate("zero", "one"), timerange),
-	sum_two: flow(extractTwoFromData("value1", "value2"), sumTwoData, timerange),
+	sum_two: flow(extractTwoFromData("value1", "value2"), sumTwoData()("data1", "data2"), timerange),
 	moving_sum: flow(extractFromData("value"), dropNullData(), makeMovingSum, timerange),
-	moving_sum_sum_two: flow(extractTwoFromData("value1", "value2"), sumTwoData, makeMovingSum, timerange),
-	simple_with_sign: flow(extractTwoFromData("value", "sign"), dropNullData("data1"), multipleWithSign("data1", "data2"), timerange),
-	moving_sum_with_sign: flow(extractTwoFromData("value", "sign"), dropNullData("data1"), multipleWithSign("data1", "data2"), makeMovingSum, timerange),
+	moving_sum_sum_two: flow(extractTwoFromData("value1", "value2"), sumTwoData()("data1", "data2"), makeMovingSum, timerange),
+	simple_with_sign: flow(extractTwoFromData("value", "sign"), dropNullData("data1"), multipleWithSign()("data1", "data2"), timerange),
+	moving_sum_with_sign: flow(extractTwoFromData("value", "sign"), dropNullData("data1"), multipleWithSign()("data1", "data2"), makeMovingSum, timerange),
 	derivation: flow(extractFromData("value"), dropNullData(), derivation, timerange),
+	telj_battery_watt: flow(...teljesitmenyek.battery_watt, round()(), timerange),
+	telj_pv: flow(...teljesitmenyek.pv, round()(), timerange),
+	telj_pv_sum: flow(...teljesitmenyek.pv_sum, round()(), timerange),
+	telj_active_power: flow(...teljesitmenyek.active_power, round()(), timerange),
+	telj_sum: flow(...teljesitmenyek.sum, round()(), timerange),
+	telj_moving_sum_battery_watt: flow(...teljesitmenyek.battery_watt, makeMovingSum, round()(), timerange),
+	telj_moving_sum_pv: flow(...teljesitmenyek.pv, makeMovingSum, round()(), timerange),
+	telj_moving_sum_pv_sum: flow(...teljesitmenyek.pv_sum, makeMovingSum, round()(), timerange),
+	telj_moving_sum_active_power: flow(...teljesitmenyek.active_power, makeMovingSum, round()(), timerange),
+	telj_moving_sum_sum: flow(...teljesitmenyek.sum, makeMovingSum, round()(), timerange),
 };
 
 export const statements = Object.fromEntries(Object.entries(queries).map(([k, v]) => {
